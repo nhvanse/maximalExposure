dd37f39f27021fd2dcb68cd33677735d6b08e4b8
van <nguyenhuuvan26101998@gmail.com>
2020-06-05 13:36:08 +0700

    Fix mutation

--- plot.py
+++ plot.py
@@ -1,20 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+mngr = plt.get_current_fig_manager()
+# to put it into the upper left corner for example:
+mngr.window.setGeometry(50, 100, 640, 545)
+
 plt.axis('equal')
-plt.axis([0,100,0,100])
+plt.axis([0, 100, 0, 100])
+
+
 def plotChart(filename):
-    output = open (filename).read()
+    output = open(filename).read()
     lines = output.split('\n')
     w, h = map(float, lines[0].split())
     number_sensors = int(lines[1])
     list_sensors = []
-    for i in range(2, number_sensors+2):
-        #print(lines[i])
+    for i in range(2, number_sensors + 2):
+        # print(lines[i])
         x, y, r = map(float, lines[i].split())
-        list_sensors.append([x,y,r])
+        list_sensors.append([x, y, r])
 
-    index = number_sensors+2
+    index = number_sensors + 2
     speed = map(float, lines[index])
 
     index += 1
@@ -24,20 +30,19 @@ def plotChart(filename):
     x_dest, y_dest = map(float, lines[index].split())
 
     index += 4
-    #print(lines[index])
+    # print(lines[index])
     num_locs = int(lines[index])
     list_locs = []
-    for i in range(index+1, index+num_locs+1):
-        x, y= map(float, lines[i].split())
-        list_locs.append([x,y])
-    #print(list_locs)
+    for i in range(index + 1, index + num_locs + 1):
+        x, y = map(float, lines[i].split())
+        list_locs.append([x, y])
+    # print(list_locs)
 
-    
     a = np.array(list_locs)
-    plt.plot(a[:,0], a[:,1])
-    #plt.plot(a[:,0], a[:,1], 'yo')
+    plt.plot(a[:, 0], a[:, 1])
+    # plt.plot(a[:,0], a[:,1], 'yo')
     plt.plot(a[0][0], a[0][1], 'bo')
-    plt.plot(a[-1][0], a[-1][1] ,'bo')
+    plt.plot(a[-1][0], a[-1][1], 'bo')
     ax = plt.gca()
     for sensor in list_sensors:
         circle = plt.Circle((sensor[0], sensor[1]), sensor[2], color='r')
@@ -46,11 +51,11 @@ def plotChart(filename):
 
 plotChart('./output/bestIndividual.txt')
 
-'''
-plotChart('./output/output1.txt')
-plotChart('./output/output2.txt')
-plotChart('./output/output3.txt')
-'''
+# plotChart('./output/output1.txt')
+# plotChart('./output/output2.txt')
+# plotChart('./output/output3.txt')
 
-plt.show()
 
+plt.show(block=False)
+plt.pause(5)
+plt.close()
--- plot_pop.py
+++ plot_pop.py
@@ -3,18 +3,19 @@ import numpy as np
 
 plt.axis('equal')
 
+
 def plotChart(filename):
-    output = open (filename).read()
+    output = open(filename).read()
     lines = output.split('\n')
     w, h = map(float, lines[0].split())
     number_sensors = int(lines[1])
     list_sensors = []
-    for i in range(2, number_sensors+2):
-        #print(lines[i])
+    for i in range(2, number_sensors + 2):
+        # print(lines[i])
         x, y, r = map(float, lines[i].split())
-        list_sensors.append([x,y,r])
+        list_sensors.append([x, y, r])
 
-    index = number_sensors+2
+    index = number_sensors + 2
     speed = map(float, lines[index])
 
     index += 1
@@ -25,25 +26,27 @@ def plotChart(filename):
 
     index += 3
     gene_length = int(lines[index])
-    
+
     index += 1
     num_individual = int(lines[index])
-    
-    for i in range(index+1, index+num_individual+1):
+
+    for i in range(index + 1, index + num_individual + 1):
         list_locs = []
         convert = lambda string: (float(string.split(',')[0]), float(string.split(',')[1]))
         xy_strings = lines[i].split()
         for string in xy_strings:
-            x, y= convert(string)
-            list_locs.append([x,y])
+            x, y = convert(string)
+            list_locs.append([x, y])
         a = np.array(list_locs)
-        plt.plot(a[:,0], a[:,1])
-    
+        plt.plot(a[:, 0], a[:, 1])
+
     ax = plt.gca()
     for sensor in list_sensors:
         circle = plt.Circle((sensor[0], sensor[1]), sensor[2], color='r')
         ax.add_artist(circle)
 
-plotChart('./output/pop.txt')
-plt.show()
 
+plotChart('./output/pop.txt')
+plt.show(block=False)
+plt.pause(5)
+plt.close()
